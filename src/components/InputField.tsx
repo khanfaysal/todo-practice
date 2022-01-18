77be99cea9d3629989e2522d86dc3968446375fb
khanfaysal <mdfaysalkhancse@gmail.com>
2022-01-18 19:06:27 +0600

    todo demo web app

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import "./styles.css";
 
-const InputField = () => {
+interface Props {
+    todo: string,
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo }) => {
     return (
         <form className='input'>
-            <input className='input__box' type="input" placeholder='Write your task name' />
+            <input className='input__box' type="input" placeholder='Write your task name' value={todo} onChange={ } />
             <button className='input__submit' type="submit">Add</button>
         </form>
     )
